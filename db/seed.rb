a8f9d1d30d8fca4939499d08445e7608771aefd0
April Rieger <april.rieger@yahoo.com>
2023-10-24 11:23:42 -0700

    Updates readme and adds seeds back after confirming configuration with soley my user

--- db/seed.rb
+++ db/seed.rb
@@ -2,32 +2,26 @@ Group.find_or_create_by(name: 'devops') do |group|
   group.description = "SoftServ DevOps Team"
 end
 
-# Group.find_or_create_by(name: 'gcsupport') do |group|
-#   group.description = "Glass Canvas Support Team"
-# end
+Group.find_or_create_by(name: 'gcsupport') do |group|
+  group.description = "Glass Canvas Support Team"
+end
 
-# Group.find_or_create_by(name: 'gcdev') do |group|
-#   group.description = "Glass Canvas Dev Team"
-# end
+Group.find_or_create_by(name: 'gcdev') do |group|
+  group.description = "Glass Canvas Dev Team"
+end
 
-# Member.find_or_create_by(handle: 'rob') do |member|
-#   member.name = "Rob Kauufman"
-#   member.group = Group.find_by(name: 'devops')
-#   member.member_id = "U0E347KGF"
-# end
-
-# Member.find_or_create_by(handle: 'crystal') do |member|
-#   member.name = "Crystal Richardson"
-#   member.group = Group.find_by(name: 'devops')
-# end
-
-# Member.find_or_create_by(handle: 'stefan') do |member|
-#   member.name = "Stefan"
-#   member.group = Group.find_by(name: 'gcdev')
-# end
-
-Member.find_or_create_by(handle: 'april') do |member|
-  member.name = "April Rieger"
+Member.find_or_create_by(handle: 'rob') do |member|
+  member.name = "Rob Kaufman"
   member.group = Group.find_by(name: 'devops')
-  member.member_id = "U02QC5MTU8N"
+  member.member_id = "U0E347KGF"
+end
+
+Member.find_or_create_by(handle: 'crystal') do |member|
+  member.name = "Crystal Richardson"
+  member.group = Group.find_by(name: 'devops')
+end
+
+Member.find_or_create_by(handle: 'stefan') do |member|
+  member.name = "Stefan"
+  member.group = Group.find_by(name: 'gcdev')
 end
