16150ab4d84dfdf935c2a2a5cc97186d60aae65d
April Rieger <april.rieger@yahoo.com>
2023-10-23 20:57:11 -0700

    chmod +x the entrypoint.sh script

--- db/seed.rb
+++ db/seed.rb
@@ -2,26 +2,32 @@ Group.find_or_create_by(name: 'devops') do |group|
   group.description = "SoftServ DevOps Team"
 end
 
-Group.find_or_create_by(name: 'gcsupport') do |group|
-  group.description = "Glass Canvas Support Team"
-end
+# Group.find_or_create_by(name: 'gcsupport') do |group|
+#   group.description = "Glass Canvas Support Team"
+# end
 
-Group.find_or_create_by(name: 'gcdev') do |group|
-  group.description = "Glass Canvas Dev Team"
-end
+# Group.find_or_create_by(name: 'gcdev') do |group|
+#   group.description = "Glass Canvas Dev Team"
+# end
 
-Member.find_or_create_by(handle: 'rob') do |member|
-  member.name = "Rob Kauufman"
+# Member.find_or_create_by(handle: 'rob') do |member|
+#   member.name = "Rob Kauufman"
+#   member.group = Group.find_by(name: 'devops')
+#   member.member_id = "U0E347KGF"
+# end
+
+# Member.find_or_create_by(handle: 'crystal') do |member|
+#   member.name = "Crystal Richardson"
+#   member.group = Group.find_by(name: 'devops')
+# end
+
+# Member.find_or_create_by(handle: 'stefan') do |member|
+#   member.name = "Stefan"
+#   member.group = Group.find_by(name: 'gcdev')
+# end
+
+Member.find_or_create_by(handle: 'april') do |member|
+  member.name = "April Rieger"
   member.group = Group.find_by(name: 'devops')
-  member.member_id = "U0E347KGF"
-end
-
-Member.find_or_create_by(handle: 'crystal') do |member|
-  member.name = "Crystal Richardson"
-  member.group = Group.find_by(name: 'devops')
-end
-
-Member.find_or_create_by(handle: 'stefan') do |member|
-  member.name = "Stefan"
-  member.group = Group.find_by(name: 'gcdev')
+  member.member_id = "U02QC5MTU8N"
 end
