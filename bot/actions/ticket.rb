493fee7b334a0bc69bb033fd65e12ef126eda921
April Rieger <april.rieger@yahoo.com>
2023-11-11 15:41:52 -0800

    Alter action for dumping payload in the scibot channel

--- bot/actions/ticket.rb
+++ bot/actions/ticket.rb
@@ -10,31 +10,17 @@ SlackRubyBotServer::Events.configure do |config|
     slack_client = Slack::Web::Client.new(token: team.token)
 
     case action_id
-    when 'reply_yes', 'reply_no'
-      # Existing quiz action handling
-      response_text = action_id == 'reply_yes' ? "Great! I also think so! :thumbsup:" : "Oops! Try to google it... :thumbsdown:"
-      slack_client.chat_postMessage(channel: channel_id, text: response_text)
     when 'create_issue'
       # Handle the create issue action for /ticket
       issue_title = payload["state"]["values"]["ticket_title"]["value"]
       issue_description = payload["state"]["values"]["ticket_description"]["value"]
-      create_github_issue(issue_title, issue_description, channel_id, user_id, slack_client)
+
+      # scibot channel id for now
+      channel_id = 'C065NSAGFAM'
+      # Go to scibot channel and put the payload there.
+      slack_client.chat_postMessage(channel: channel_id, text: "Issue title: #{issue_title}\nIssue description: #{issue_description}")
     end
 
     nil
-  end
-end
-
-def create_github_issue(title, description, channel_id, user_id, slack_client)
-  github_token = ENV['GITHUB_TOKEN']
-  github_repository = ENV['GITHUB_REPOSITORY']
-
-  github_client = Octokit::Client.new(access_token: github_token)
-
-  begin
-    issue = github_client.create_issue(github_repository, title, description)
-    slack_client.chat_postMessage(channel: channel_id, text: "Hi <@#{user_id}>, your issue has been created: #{issue.html_url}")
-  rescue => e
-    slack_client.chat_postMessage(channel: channel_id, text: "Sorry <@#{user_id}>, there was an error creating your issue: #{e.message}")
   end
 end
